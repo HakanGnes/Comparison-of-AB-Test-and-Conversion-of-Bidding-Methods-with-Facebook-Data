52184dc1fa29991c010b88f8c33dcd72508f9133
HakanGnes <100923570+HakanGnes@users.noreply.github.com>
2023-01-10 13:43:43 +0300

    Create AB_testi case study.py
    AB Testi ile Bidding Yöntemlerinin
    Dönüşümünün Karşılaştırılması

---
+++ AB_testi case study.py
@@ -0,0 +1,161 @@
+#####################################################
+# AB Testi ile Bidding Yöntemlerinin Dönüşümünün Karşılaştırılması
+#####################################################
+
+#####################################################
+# İş Problemi
+#####################################################
+
+# Facebook kısa süre önce mevcut "maximumbidding" adı verilen teklif verme türüne alternatif
+# olarak yeni bir teklif türü olan "average bidding"’i tanıttı. Müşterilerimizden biri olanbombabomba.com,
+# bu yeni özelliği test etmeye karar verdi veaveragebidding'in maximumbidding'den daha fazla dönüşüm
+# getirip getirmediğini anlamak için birA/B testiyapmak istiyor.A/B testi 1 aydır devam ediyor ve
+# bombabomba.com şimdi sizden bu A/B testinin sonuçlarını analiz etmenizi bekliyor.Bombabomba.com için
+# nihai başarı ölçütü Purchase'dır. Bu nedenle, istatistiksel testler için Purchasemetriğine odaklanılmalıdır.
+
+
+#####################################################
+# Veri Seti Hikayesi
+#####################################################
+
+# Bir firmanın web site bilgilerini içeren bu veri setinde kullanıcıların gördükleri ve tıkladıkları
+# reklam sayıları gibi bilgilerin yanı sıra buradan gelen kazanç bilgileri yer almaktadır.Kontrol ve Test
+# grubu olmak üzere iki ayrı veri seti vardır. Bu veri setleri ab_testing.xlsx excel’inin ayrı sayfalarında yer
+# almaktadır. Kontrol grubuna Maximum Bidding, test grubuna Average Bidding uygulanmıştır.
+
+# impression: Reklam görüntüleme sayısı
+# Click: Görüntülenen reklama tıklama sayısı
+# Purchase: Tıklanan reklamlar sonrası satın alınan ürün sayısı
+# Earning: Satın alınan ürünler sonrası elde edilen kazanç
+
+#####################################################
+# Proje Görevleri
+#####################################################
+
+#####################################################
+# Görev 1:  Veriyi Hazırlama ve Analiz Etme
+#####################################################
+
+# Adım 1:  ab_testing_data.xlsx adlı kontrol ve test grubu verilerinden oluşan veri setini okutunuz. Kontrol ve test grubu verilerini ayrı değişkenlere atayınız.
+
+# Gerekli olabilecek kütüphaneler
+import itertools
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+# !pip install statsmodels
+import statsmodels.stats.api as sms
+from scipy.stats import ttest_1samp, shapiro, levene, ttest_ind, mannwhitneyu, \
+    pearsonr, spearmanr, kendalltau, f_oneway, kruskal
+from statsmodels.stats.proportion import proportions_ztest
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', 10)
+pd.set_option('display.float_format', lambda x: '%.5f' % x)
+
+df1_control = pd.read_excel("measurement_problems/ABTesti/ab_testing.xlsx", sheet_name="Control Group")
+df2_test = pd.read_excel("measurement_problems/ABTesti/ab_testing.xlsx", sheet_name="Test Group")
+df1_control.head()
+df2_test.head()
+
+df_control = df1_control.copy()
+df_test = df2_test.copy()
+
+
+# Adım 2: Kontrol ve test grubu verilerini analiz ediniz.
+
+def check_df(dataframe, head=5):
+    print("                   Shape               ")
+    print(dataframe.shape)
+    print("                   Types               ")
+    print(dataframe.dtypes)
+    print("                    Head               ")
+    print(dataframe.head())
+    print("                    Tail               ")
+    print(dataframe.tail())
+    print("                     NA                ")
+    print(dataframe.isnull().sum())
+    print("                  Quantiles            ")
+    print(dataframe.quantile([0, 0.25, 0.50, 0.75, 0.95, 0.99, 1]).T)
+
+
+check_df(df_control)
+check_df(df_test)
+
+# Adım 3: Analiz işleminden sonra concat metodunu kullanarak kontrol ve test grubu verilerini birleştiriniz.
+
+df_control["group"] = "control"
+df_test["group"] = "test"
+
+df = pd.concat([df_control,df_test], axis=0,ignore_index=False)
+df.head()
+df.sort_values(by="group")
+
+#####################################################
+# Görev 2:  A/B Testinin Hipotezinin Tanımlanması
+#####################################################
+
+# Adım 1: Hipotezi tanımlayınız.
+
+# H0 : M1 = M2
+# Kontrol grubu ve test grubu satın alma ortalamaları arasında fark yoktur.
+# H1 : M1!= M2
+# Kontrol grubu ve test grubu satın alma ortalamaları arasında fark vardır.
+
+# Adım 2: Kontrol ve test grubu için purchase(kazanç) ortalamalarını analiz ediniz
+
+df.groupby("group").agg({"Purchase": "mean"})
+
+#####################################################
+# GÖREV 3: Hipotez Testinin Gerçekleştirilmesi
+#####################################################
+
+# Adım 1: Hipotez testi yapılmadan önce varsayım kontrollerini yapınız.Bunlar Normallik Varsayımı ve Varyans Homojenliğidir.
+
+# Kontrol ve test grubunun normallik varsayımına uyup uymadığını Purchase değişkeni üzerinden ayrı ayrı test ediniz
+# Normallik Varsayımı :
+# H0: Normal dağılım varsayımı sağlanmaktadır.
+# H1: Normal dağılım varsayımı sağlanmamaktadır
+# p < 0.05 H0 RED
+# p > 0.05 H0 REDDEDİLEMEZ
+# Test sonucuna göre normallik varsayımı kontrol ve test grupları için sağlanıyor mu ?
+# Elde edilen p-value değerlerini yorumlayınız.
+
+
+test_stat, pvalue = shapiro(df.loc[df["group"] == "control", "Purchase"])
+print('Test Stat = %.4f, p-value = %.4f' % (test_stat, pvalue))
+# p-value=0.5891
+# HO reddedilemez. Control grubunun değerleri normal dağılım varsayımını sağlamaktadır.
+
+# Varyans Homojenliği :
+# H0: Varyanslar homojendir.
+# H1: Varyanslar homojen değildir.
+# p < 0.05 H0 RED
+# p > 0.05 H0 REDDEDİLEMEZ
+# Kontrol ve test grubu için varyans homojenliğinin sağlanıp sağlanmadığını Purchase değişkeni üzerinden test ediniz.
+
+
+test_stat, pvalue = levene(df.loc[df["group"] == "control", "Purchase"],
+                           df.loc[df["group"] == "test", "Purchase"])
+print('Test Stat = %.4f, p-value = %.4f' % (test_stat, pvalue))
+# p-value=0.1083
+# HO reddedilemez. Control ve Test grubunun değerleri varyans homejenliği varsayımını sağlamaktadır.
+# Varyanslar Homojendir.
+
+# Adım 2: Normallik Varsayımı ve Varyans Homojenliği sonuçlarına göre uygun testi seçiniz
+
+# Varsayımlar sağlandığı için bağımsız iki örneklem t testi (parametrik test) yapılmaktadır.
+# H0: M1 = M2 (Kontrol grubu ve test grubu satın alma ortalamaları arasında ist. ol.anl.fark yoktur.)
+# H1: M1 != M2 (Kontrol grubu ve test grubu satın alma ortalamaları arasında ist. ol.anl.fark vardır)
+# p<0.05 HO RED , p>0.05 HO REDDEDİLEMEZ
+
+test_stat, pvalue = ttest_ind(df.loc[df["group"] == "control", "Purchase"],
+                              df.loc[df["group"] == "test", "Purchase"],
+                              equal_var=True)
+
+print('Test Stat = %.4f, p-value = %.4f' % (test_stat, pvalue))
+# p-value = 0.3493
+# HO reddedilemez.
+# Kontrol grubu ve test grubu satın alma ortalamaları arasında ist. ol.anl.fark yoktur.
+
